78336f2bfd6945fbdbf692fe2bd32ffd37cf6412
velakuruday <velakuruday@gmail.com>
2022-08-30 23:11:26 +0530

    Added Es congig

---
+++ src/main/java/com/indi/eventapi/configuration/elasticsearch/ElasticsearchConfig.java
@@ -0,0 +1,24 @@
+package com.indi.eventapi.configuration.elasticsearch;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ElasticsearchConfig {
+
+    @Bean
+    public ElasticsearchClient getElasticsearchClient() {
+        RestClient restClient = RestClient.builder(new HttpHost("localhost", 9200)).build();
+
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+        return new ElasticsearchClient(transport);
+    }
+}
