6fa029ab306f7727a7efc04ee08ce441df781bd0
velakuruday <velakuruday@gmail.com>
2024-08-22 18:55:51 +0530

    Added models

--- src/main/java/com/indi/eventapi/models/Stock.java
+++ src/main/java/com/indi/eventapi/models/Stock.java
@@ -1,7 +1,9 @@
 package com.indi.eventapi.models;
 
+import com.indi.eventapi.dto.StockDto;
 import lombok.Builder;
 import lombok.Data;
+import org.rocksdb.Status;
 
 @Data
 @Builder
@@ -23,4 +25,16 @@ public class Stock {
 
     private Integer volume;
 
+    public static Stock toStock(StockDto stockDto) {
+        return Stock.builder()
+                .company(CodeMap.codeMap.get(stockDto.getCode()))
+                .code(stockDto.getCode())
+                .adjClose(stockDto.getAdjClose())
+                .open(stockDto.getOpen())
+                .close(stockDto.getClose())
+                .high(stockDto.getHigh())
+                .low(stockDto.getLow())
+                .volume(stockDto.getVolume())
+                .build();
+    }
 }
--- src/main/java/com/indi/eventapi/models/StockUpdate.java
+++ src/main/java/com/indi/eventapi/models/StockUpdate.java
@@ -1,9 +1,11 @@
 package com.indi.eventapi.models;
 
+import com.indi.eventapi.dto.StockUpdateDto;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -12,4 +14,11 @@ public class StockUpdate {
     private String timestamp;
 
     private List<Stock> stocks;
+
+    public static StockUpdate toStockUpdate(StockUpdateDto stockUpdateDto) {
+        return StockUpdate.builder()
+                .timestamp(stockUpdateDto.getTimestamp())
+                .stocks(stockUpdateDto.getStocks().stream().map(Stock::toStock).collect(Collectors.toList()))
+                .build();
+    }
 }
